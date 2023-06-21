2ce44f9a577223319fb4a3cb24e36370eadf0060
Chern Shee Edison Chong <s10223557@connect.np.edu.sg>
2023-06-21 19:21:21 +0800

    updated bus arrival feature

---
+++ app/src/main/java/sg/edu/np/mad/madassignmentteam1/BusArrivalViewHolder.java
@@ -0,0 +1,24 @@
+package sg.edu.np.mad.madassignmentteam1;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BusArrivalViewHolder extends RecyclerView.ViewHolder {
+    public TextView busStopCodeTextView;
+    public TextView serviceNoTextView;
+    public TextView nextBusTextView;
+    public TextView nextBus2TextView;
+    public TextView nextBus3TextView;
+
+    public BusArrivalViewHolder(View item) {
+        super(item);
+        busStopCodeTextView = item.findViewById(R.id.busStopCodeTextView);
+        serviceNoTextView = item.findViewById(R.id.serviceNoTextView);
+        nextBusTextView = item.findViewById(R.id.nextBusTextView);
+        nextBus2TextView = item.findViewById(R.id.nextBus2TextView);
+        nextBus3TextView = item.findViewById(R.id.nextBus3TextView);
+    }
+}
+
